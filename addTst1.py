bd9dc7a6da14d2cc09f7dba7fffaacaac1df5c48
SchiffS <40877004+SchiffS@users.noreply.github.com>
2018-07-17 10:00:15 -0400

    Update addTst1.py

--- addTst1.py
+++ addTst1.py
@@ -1,20 +1,2 @@
 #addTst1(A,B):  test of matrix addition under numpy
-
-import numpy as np
-
-def addTst1(A,B):
-    """Test of matrix addition under numpy. A,B are of class ndarray"""
-    try:
-        C = A + B
-    except ValueError:
-        if np.shape(A) != np.shape(B):
-            print('ValueError raised . . Correctly! Reload script for more')
-            return(0)
-  
-    print('returns\n', C)
-    if np.shape(A) != np.shape(B):
-        print('\nThis is mathematically erroneous\n')
-    else: print('\nThis is mathematically correct\n')
-    
-    return(0)
-    
+#file deleted
